191c4e49fedd09e7628aa0615a3bfb68d7558ac2
nirmalakarim09 <nirmalarizkakarim09@gmail.com>
2026-07-08 11:44:17 +0900

    Update cctv.js

--- js/cctv.js
+++ js/cctv.js
@@ -15,115 +15,115 @@ const cctvs = [
     nama:"CCTV Depan ASMIL OSM",
     lat:-3.705,
     lng:128.16702,
-    stream:"http://localhost:8889/cctv02"
+    stream:"http://174.1.1.50:8889/cctv02"
 },
 {
     nama:"CCTV Depan Balai Kota",
     lat:-3.69421,
     lng:128.18149,
-    stream:"http://localhost:8889/cctv03"
+    stream:"http://174.1.1.50:8889/cctv03"
 },
 {
     nama:"CCTV 05",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv05"
+    stream:"http://174.1.1.50:8889/cctv05"
 },
 {
     nama:"CCTV 06",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv06"
+    stream:"http://174.1.1.50:8889/cctv06"
 },
 {
     nama:"CCTV 07",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv07"
+    stream:"http://174.1.1.50:8889/cctv07"
 },
 {
     nama:"CCTV 08",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv08"
+    stream:"http://174.1.1.50:8889/cctv08"
 },
 {
     nama:"CCTV 09",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv09"
+    stream:"http://174.1.1.50:8889/cctv09"
 },
 {
     nama:"CCTV 10",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv10"
+    stream:"http://174.1.1.50:8889/cctv10"
 },
 {
     nama:"CCTV 11",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv11"
+    stream:"http://174.1.1.50:8889/cctv11"
 },
 {
     nama:"CCTV 12",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv12"
+    stream:"http://174.1.1.50:8889/cctv12"
 },
 {
     nama:"CCTV 13",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv13"
+    stream:"http://174.1.1.50:8889/cctv13"
 },
 {
     nama:"CCTV 14",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv14"
+    stream:"http://174.1.1.50:8889/cctv14"
 },
 {
     nama:"CCTV 15",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv15"
+    stream:"http://174.1.1.50:8889/cctv15"
 },
 {
     nama:"CCTV 16",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv16"
+    stream:"http://174.1.1.50:8889/cctv16"
 },
 {
     nama:"CCTV 17",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv17"
+    stream:"http://174.1.1.50:8889/cctv17"
 },
 {
     nama:"CCTV 18",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv18"
+    stream:"http://174.1.1.50:8889/cctv18"
 },
 {
     nama:"CCTV 19",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv19"
+    stream:"http://174.1.1.50:8889/cctv19"
 },
 {
     nama:"CCTV 20",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv20"
+    stream:"http://174.1.1.50:8889/cctv20"
 },
 {
     nama:"CCTV 21",
     lat:-3.6930,
     lng:128.1785,
-    stream:"http://localhost:8889/cctv21"
+    stream:"http://174.1.1.50:8889/cctv21"
 }
 
 
